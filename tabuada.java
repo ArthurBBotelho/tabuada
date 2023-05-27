b0dede466739eb08120219463388bcbb128c7eab
Arthur Botelho <emilioarthur19@gmail.com>
2023-05-27 11:17:30 -0300

    tabuada concuida de 1 até 10

---
+++ tabuada.java
@@ -0,0 +1,16 @@
+public class tabuada {
+
+    public static void main(String[] args){
+        // tabuada de multiplicação usando laço for de 1 até 10
+
+        for ( int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                // resultado de exemplo na tela será 1 x 1 = 1
+                System.out.println(j + " x " + i + " = " + i * j);
+            }
+        }
+
+
+    }
+
+}
